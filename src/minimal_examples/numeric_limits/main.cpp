419a870754aa45df45bbe14c6f79b4538434c0a4
Matt Nowak <17482065+Tuee22@users.noreply.github.com>
2024-01-02 17:12:56 -0500

    optim

--- src/minimal_examples/numeric_limits/main.cpp
+++ src/minimal_examples/numeric_limits/main.cpp
@@ -4,6 +4,9 @@
 #include <vector>
 #include <limits>
 
+#define TYPE unsigned short
+#define max 65535
+
 class numeric_test_no_init
 {
     public:
@@ -12,7 +15,7 @@ class numeric_test_no_init
             return;
         }
     private:
-        unsigned long _l;
+        TYPE _l;
 };
 
 class numeric_test_zero_init
@@ -20,10 +23,10 @@ class numeric_test_zero_init
     public:
         numeric_test_zero_init() noexcept
         {
-            _l=0;
+            _l=max;
         }
     private:
-        unsigned short _l;
+        TYPE _l;
 };
 
 class numeric_test_max_init
@@ -31,32 +34,36 @@ class numeric_test_max_init
     public:
         numeric_test_max_init() noexcept
         {
-            _l=std::numeric_limits<unsigned short>::max();
+            _l=std::numeric_limits<TYPE>::max();
         }
     private:
-        unsigned long _l;
+        TYPE _l;
 };
 
 int main() {
     
-    size_t i = 1000000000;
+    size_t inner_size = 1000000;
+    size_t outer_size = 1000;
     clock_t begin, end;
     double elapsed_secs;
 
     begin = clock();
-    std::vector<numeric_test_no_init> no_init(i);
+    for (size_t i=0;i<outer_size;++i)
+        std::vector<numeric_test_no_init> no_init(inner_size);
     end = clock();
     elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
     std::cout << "numeric_test_no_init took " << elapsed_secs << " seconds." << std::endl;
     
     begin = clock();
-    std::vector<numeric_test_zero_init> zero_init(i);
+    for (size_t i=0;i<outer_size;++i)
+        std::vector<numeric_test_zero_init> zero_init(inner_size);
     end = clock();
     elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
     std::cout << "numeric_test_zero_init took " << elapsed_secs << " seconds." << std::endl;
 
     begin = clock();
-    std::vector<numeric_test_max_init> max_init(i);
+    for (size_t i=0;i<outer_size;++i)
+        std::vector<numeric_test_max_init> max_init(inner_size);
     end = clock();
     elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
     std::cout << "numeric_test_max_init took " << elapsed_secs << " seconds." << std::endl;
